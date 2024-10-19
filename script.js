104ef6f337ecb0e4305c3d6044aa12e6bf23ffff
PeteK4 <petek2012@outlook.com>
2024-10-19 10:14:44 +0300

    Päivitetty

--- script.js
+++ script.js
@@ -18,23 +18,15 @@ function aikaLaskuri() {
     document.getElementById('tunnit').innerHTML = tunnit;
     document.getElementById('minuutit').innerHTML = minuutit;
     document.getElementById('sekunnit').innerHTML = sekunnit;
-    
-    console.log(paivat)
     document.getElementById('otsikko').innerHTML = "Jouluaattoon on aikaa"
+
     if (paivat == 364) {
         document.getElementById('otsikko').innerHTML = "Nyt on jouluaatto"
         document.getElementById('hyvaaJoulua').innerHTML = "Hyvää ja Rauhallista Joulua!"
         document.getElementById('yot').style = "display: none"
         document.getElementById('yo').style = "display: none"
-        document.getElementById('paivat').style = "display: none"
-        document.getElementById('pva').style = "display: none"
-        document.getElementById('tunnit').style = "display: none"
-        document.getElementById('tun').style = "display: none"
-        document.getElementById('minuutit').style = "display: none"
-        document.getElementById('min').style = "display: none"
-        document.getElementById('sekunnit').style = "display: none"
-        document.getElementById('sek').style = "display: none"
-
+        document.getElementById('ptms').style = "display: none"
+        document.getElementById('valiLause').style = "display: none"
     }
 
     if (paivat == 0) {
