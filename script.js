b68226e9d23e169fa383cf1a0029ae323026cff2
PeteK4 <petek2012@outlook.com>
2024-10-19 10:14:28 +0300

    Päivitetty

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function aikaLaskuri() {
     const nytHetki = new Date();
     const kuluvaVuosi = nytHetki.getFullYear();
-    const jouluaatto = new Date(`Decenber 24, ${kuluvaVuosi} 00:00:00`);
+    const jouluaatto = new Date(`December 24, ${kuluvaVuosi} 00:00:00`);
 
     if (nytHetki > jouluaatto) {
         jouluaatto.setFullYear(kuluvaVuosi + 1);
@@ -18,6 +18,24 @@ function aikaLaskuri() {
     document.getElementById('tunnit').innerHTML = tunnit;
     document.getElementById('minuutit').innerHTML = minuutit;
     document.getElementById('sekunnit').innerHTML = sekunnit;
+    
+    console.log(paivat)
+    document.getElementById('otsikko').innerHTML = "Jouluaattoon on aikaa"
+    if (paivat == 364) {
+        document.getElementById('otsikko').innerHTML = "Nyt on jouluaatto"
+        document.getElementById('hyvaaJoulua').innerHTML = "Hyvää ja Rauhallista Joulua!"
+        document.getElementById('yot').style = "display: none"
+        document.getElementById('yo').style = "display: none"
+        document.getElementById('paivat').style = "display: none"
+        document.getElementById('pva').style = "display: none"
+        document.getElementById('tunnit').style = "display: none"
+        document.getElementById('tun').style = "display: none"
+        document.getElementById('minuutit').style = "display: none"
+        document.getElementById('min').style = "display: none"
+        document.getElementById('sekunnit').style = "display: none"
+        document.getElementById('sek').style = "display: none"
+
+    }
 
     if (paivat == 0) {
         document.getElementById('yo').innerHTML = " yö"
